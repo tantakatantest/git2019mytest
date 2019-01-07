358f3088b8cd63a8d331599bf3075532906074ca
tantakatantest <htghlisutuchie@gmail.com>
2019-01-07 16:49:43 +0900

    three messages added

--- Sample01.java
+++ Sample01.java
@@ -1,5 +1,6 @@
 public class Sample01{
 	public static void main(String[] args){
-		System.out.println("Hello Git");
+		System.out.println("Hello Git 2019");
+		System.out.println("Hello Gi");
 	}
 }
